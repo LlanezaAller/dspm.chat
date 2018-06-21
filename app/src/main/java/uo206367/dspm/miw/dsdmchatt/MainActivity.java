f7cb5b2c0a5756a288ee1d64a25d2446cc8c30e7
Unknown <uo206367@uniovi.es>
2018-06-21 21:49:53 +0200

    Project Creation
    Project Creation

---
+++ app/src/main/java/uo206367/dspm/miw/dsdmchatt/MainActivity.java
@@ -0,0 +1,14 @@
+package uo206367.dspm.miw.dsdmchatt;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
