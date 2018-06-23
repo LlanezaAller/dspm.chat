da36812fd032c797361ef960c044e7644f0a2ef7
Unknown <uo206367@uniovi.es>
2018-06-23 22:47:42 +0200

    Message Reception ready
    (Message format TODO next)

--- app/src/main/java/uo206367/dspm/miw/dsdmchatt/LoginFragment.java
+++ app/src/main/java/uo206367/dspm/miw/dsdmchatt/LoginFragment.java
@@ -13,16 +13,7 @@ public class LoginFragment extends Fragment {
     public LoginFragment() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
+    public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
         return fragment;
     }
--- app/src/main/java/uo206367/dspm/miw/dsdmchatt/MainActivity.java
+++ app/src/main/java/uo206367/dspm/miw/dsdmchatt/MainActivity.java
@@ -6,7 +6,25 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okhttp3.internal.Util;
+import okio.ByteString;
+import uo206367.dspm.miw.dsdmchatt.model.Data;
+import uo206367.dspm.miw.dsdmchatt.model.Message;
 import uo206367.dspm.miw.dsdmchatt.model.User;
 import uo206367.dspm.miw.dsdmchatt.service.AppDatabase;
 import uo206367.dspm.miw.dsdmchatt.util.MessageService;
@@ -15,17 +33,35 @@ public class MainActivity extends Activity {
 
     private User userLogged;
     private Fragment loginFragment;
+    private Fragment chatFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gson = new Gson();
 
         // Begin the transaction
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         // Replace the contents of the container with the new fragment
         loginFragment = new LoginFragment();
+        chatFragment = new ChatFragment();
+
         ft.add(R.id.login_fragment, loginFragment);
+
+        output = findViewById(R.id.output);
+        input = findViewById(R.id.input);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(0, TimeUnit.SECONDS);
+
+        client = new OkHttpClient.Builder()
+                //.connectTimeout(0, TimeUnit.SECONDS)
+                //.writeTimeout(0, TimeUnit.SECONDS)
+                //.readTimeout(0, TimeUnit.SECONDS)
+                .pingInterval(5000, TimeUnit.MILLISECONDS)
+                .build();
+
         // or ft.add(R.id.your_placeholder, new FooFragment());
         // Complete the changes added above
         ft.commit();
@@ -33,6 +69,7 @@ public class MainActivity extends Activity {
 
     @Override
     public void onDestroy() {
+        ws.close(NORMAL_CLOSURE_STATUS, "Bye");
         super.onDestroy();
     }
 
@@ -49,7 +86,7 @@ public class MainActivity extends Activity {
                 if (userFromDatabase != null) {
                     //User userFromRest = doLogin(userFromDatabase, view);
                     MessageService.createToast(view, R.string.login_connected);
-
+                    userLogged = userFromDatabase;
                     FragmentTransaction ft = getFragmentManager().beginTransaction();
                     ft.remove(loginFragment);
                     ft.replace(R.id.chat_fragment, new ChatFragment());
@@ -57,10 +94,10 @@ public class MainActivity extends Activity {
                 } else {
                     MessageService.createToast(view, R.string.login_error_information);
                 }
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 MessageService.createToast(view, R.string.login_error_db);
             }
-        } else{
+        } else {
             MessageService.createToast(view, R.string.login_error_information);
         }
 
@@ -76,5 +113,103 @@ public class MainActivity extends Activity {
             AppDatabase.getAppDatabase(view.getContext()).userDao().insertAll(user);
         }
 
+    }
+
+
+
+
+    private final String URL_WS = "ws://156.35.98.50:3000";
+    //private final String URL_WS = "ws://127.0.0.1:3000";
+    //components
+    private Button start;
+    public TextView output;
+    public EditText input;
+    private OkHttpClient client;
+    private Gson gson;
+
+    private static final int NORMAL_CLOSURE_STATUS = 1000;
+    private final class EchoWebSocketListener extends WebSocketListener {
+
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            //super.onOpen(webSocket, response);
+            Message msg = new Message("people", new Data("connected", userLogged.getUserName(), userLogged.getUserName(), ""));
+            String message = gson.toJson(msg);
+            webSocket.send(message);
+
+
+            if(ws == null)
+                ws = webSocket;
+            //webSocket.close(NORMAL_CLOSURE_STATUS, "Bye");
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            output("Receiving : " + text);
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            output("Receiving bytes : " + bytes.hex());
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            //super.onClosing(webSocket, code, reason);
+            closeConnection();
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            //super.onFailure(webSocket, t, response);
+            output("Error : " + t.getMessage());
+        }
+    }
+
+    private WebSocket ws;
+    private EchoWebSocketListener listener;
+
+    public void start(View view) {
+        if (ws != null && ws.request() != null) {
+            closeConnection();
+        }
+            Request request = new Request.Builder().url(URL_WS).build();
+            listener = new EchoWebSocketListener();
+            ws = client.newWebSocket(request, listener);
+
+
+            client.dispatcher().executorService();
+    }
+
+    public void send(View view) {
+        if (input == null)
+            input = findViewById(R.id.input);
+
+        sendMessage(input.getText().toString());
+    }
+
+    private void sendMessage(String text) {
+        String message = gson.toJson(new Message(userLogged.getUserName(), text));
+        ws.send(message);
+    }
+
+    private void output(final String txt) {
+        if (output == null)
+            output = findViewById(R.id.output);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(output.getText().toString() + "\n\n" + txt);
+            }
+        });
+    }
+
+    private void closeConnection(){
+        Message msg = new Message("people", new Data("disconnected", userLogged.getUserName(), userLogged.getUserName(), ""));
+        String message = gson.toJson(msg);
+        ws.send(message);
+        ws.close(NORMAL_CLOSURE_STATUS, "Bye");
     }
 }
